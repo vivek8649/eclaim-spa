b7caf1ba0954bba37f77d57951258512f87ae332
Vivekanand <vivek8649@gmail.com>
2025-10-28 00:58:29 +0530

    added register and login feature

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { Nav } from './nav/nav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Nav],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+  showNav = true;
   protected readonly title = signal('eclaim-spa');
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.showNav = !['/login', '/register'].includes(event.url);
+      }
+    });
+  }
+  
 }

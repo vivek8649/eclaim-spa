ebfd8a86b7a03c84a0137d5112c9a8572666b290
Vivekanand <vivek8649@gmail.com>
2025-10-28 00:53:06 +0530

    added register and login feature

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)},
+    {path: 'register', loadComponent: () => import('./register/register.component').then(m => m.RegisterComponent)},
+    {path: '', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
+    {path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent)},
+];
